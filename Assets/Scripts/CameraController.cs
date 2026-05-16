0bfad8c133d57b228e23e5f1e07c86951a4d260a
GusTheGreat1367 <guspearson78@gmail.com>
2026-05-15 19:50:19 -0700

    changed camera

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -2,15 +2,18 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public float XOffset = 0f;
+    public float YOffset = 0f;
+    public float ZOffset = 0f;
+    GameObject car;
+    void Awake()
     {
-        
+        transform.position += new Vector3(car.transform.position.x + XOffset, car.transform.position.y + YOffset, car.transform.position.z + ZOffset);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position += new Vector3(car.transform.position.x + XOffset, car.transform.position.y + YOffset, car.transform.position.z + ZOffset);
     }
 }
